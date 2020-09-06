cb18ec2d60487102dda909b3afe56f034f0b655b
Ekene Illoh <ekeneilloh1762@gmail.com>
2020-09-06 17:20:22 -0400

    Final commit

---
+++ TIp CalculatorTests/TIp_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  TIp_CalculatorTests.swift
+//  TIp CalculatorTests
+//
+//  Created by Ekenedilichukwu Illoh on 8/24/20.
+//  Copyright © 2020 illoh.py. All rights reserved.
+//
+
+import XCTest
+@testable import TIp_Calculator
+
+class TIp_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
